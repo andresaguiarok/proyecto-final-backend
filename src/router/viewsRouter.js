7fcfccd4f581b6bc96cf345ec62a21c7e67fdb0d
andresaguiarok <aguiarandresok@outlook.es>
2023-04-16 19:31:54 -0300

    agregamos el home.handlebars

--- src/router/viewsRouter.js
+++ src/router/viewsRouter.js
@@ -1,10 +1,19 @@
 const { Router } = require ("express")
+const ProductManager = require("../managers/productManager.js")
 
 const consulta = async()=>  {
+    const pm = new ProductManager()
     const router = Router()
 
-    router.get("/", (req, res)=>{
-        res.render("home", {})
+    router.get("/", async(req, res) => {
+        let products = await pm.getProducts()
+
+        prodManag = {
+            title: "Products",
+            products,
+            style: "index.css"
+        }
+        res.render("home", prodManag)
     })
 
     module.exports = router
