2b2e3101844e60c9ea330def5f360c2940943981
andresaguiarok <andresaguiarok@outlook.com>
2023-06-28 19:04:22 -0300

    ordenamos arcihivos objetConfig y sessionRouter

--- src/config/objetConfig.js
+++ src/config/objetConfig.js
@@ -1,4 +1,3 @@
-const { connect } = require("mongoose")
 const dotenv = require("dotenv")
 const { commander } = require("../utils/commander")
 const MongoSingleton = require("../utils/singleton")
@@ -6,14 +5,8 @@ const MongoSingleton = require("../utils/singleton")
 const { mode } = commander.opts()
 dotenv.config({ path: mode === 'development' ? './.env.development' : './.env.production' })
 
-let url = process.env.MONGO_KEY_SECRET
-
 module.exports = {
     port: process.env.PORT,
     JwtKeySecret: process.env.JWT_KEY_SECRET,
-    // connectDB: ()=> {
-    //     connect(url)
-    //     console.log("Base de datos conectada")
-    // },
     connectDB: async() => await MongoSingleton.getInstance()
 }
