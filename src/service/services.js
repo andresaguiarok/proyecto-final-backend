66d4b50732fe82bc879166b4a7762b111323a06b
andresaguiarok <andresaguiarok@outlook.com>
2023-07-03 22:51:47 -0300

    agregamos factory al service

--- src/service/services.js
+++ src/service/services.js
@@ -1,13 +1,4 @@
 const { UserDao, ProductDao, CartDao } = require("../dao/factory.js");
-// const cartManager = require("../dao/fileSystem/cartManager.js");
-// const ProductManager = require("../dao/fileSystem/productManager.js");
-// const UserManager = require("../dao/fileSystem/userManager.js");
-
-// const CartManagerMongo = require("../dao/mongoDb/cartManagerMongo.js");
-// const ProductManagerMongo = require("../dao/mongoDb/productManagerMongo.js");
-// const UserManagerMongo = require("../dao/mongoDb/userManagerMongo.js");
-
-
 const userService = UserDao
 const productService = ProductDao
 const cartService = CartDao
