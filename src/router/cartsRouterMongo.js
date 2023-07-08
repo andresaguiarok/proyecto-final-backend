f7f4c7364c4bcf24395f9062915ff44024ef7031
andresaguiarok <andresaguiarok@outlook.com>
2023-07-08 01:08:26 -0300

    delimitamos el acceso a las rutas de CartRouter

--- src/router/cartsRouterMongo.js
+++ src/router/cartsRouterMongo.js
@@ -10,14 +10,14 @@ router.get("/" , passportCall("jwt"), authorization("admin"),cartController.getC
 
 router.get("/:cid" , passportCall("jwt"), cartController.getCart)
 
-router.post("/" , cartController.createCart)
+router.post("/" , passportCall("jwt"), authorization("admin"),cartController.createCart)
 
 router.put("/:cid/products/:pid" , passportCall("jwt"), authorization("user"), cartController.addProduct)
 
 router.delete("/:cid/product/:pid" , passportCall("jwt"), authorization("user"), cartController.deleteProductInCart)
 
-router.post("/:cid/purchase", passportCall("jwt"), cartController.cartPurchase)
+router.post("/:cid/purchase", passportCall("jwt"), authorization("user"), cartController.cartPurchase)
 
-router.delete("/:cid", cartController.deleteProductsInCart)
+router.delete("/:cid", passportCall("jwt"), authorization("user"),cartController.deleteProductsInCart)
 
 module.exports = router
