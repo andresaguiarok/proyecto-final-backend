b8f04f89404b7af1732a0a65ff1ecc1b90b45d58
andresaguiarok <andresaguiarok@outlook.com>
2023-07-26 22:22:06 -0300

    modificacion para varios roles

--- src/passportJwt/authorization.js
+++ src/passportJwt/authorization.js
@@ -1,6 +1,5 @@
 const authorization = roles => {
     return async (req, res, next) => {
-        console.log(roles);
         const getRole = roles.find(role => role == req.user.role)
         if(!req.user) return res.status(401).send({status: "error", message:"Unauthorized"})
         if(req.user.role !== getRole){
