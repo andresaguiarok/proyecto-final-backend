a8eb93824a0c26891efd3fb5e90245aa31095c2a
andresaguiarok <andresaguiarok@outlook.com>
2023-08-06 20:32:00 -0300

    configuracion de autorizacion de roles de usuarios

--- src/router/cartsRouter.js
+++ src/router/cartsRouter.js
@@ -6,18 +6,18 @@ const CartController     = require("../controllers/cartsController")
 const router             = Router()
 const cartController     = new CartController()
 
-router.get("/" , passportCall("jwt"), authorization("admin"), cartController.getCarts)
+router.get("/" , passportCall("jwt"), authorization(["admin"]), cartController.getCarts)
 
 router.get("/:cid" , passportCall("jwt"), cartController.getCart)
 
-router.post("/" , passportCall("jwt"), authorization("admin"), cartController.createCart)
+router.post("/" , passportCall("jwt"), authorization(["admin"]), cartController.createCart)
 
-router.put("/:cid/products/:pid" , passportCall("jwt"), authorization("user"), cartController.addProduct)
+router.put("/:cid/products/:pid" , passportCall("jwt"), authorization(["user", "premium"]), cartController.addProduct)
 
-router.delete("/:cid/product/:pid" , passportCall("jwt"), authorization("user"), cartController.deleteProductInCart)
+router.delete("/:cid/products/:pid" , passportCall("jwt"), authorization(["user", "premium"]), cartController.deleteProductInCart)
 
-router.post("/:cid/purchase", passportCall("jwt"), authorization("user"), cartController.cartPurchase)
+router.post("/:cid/purchase", passportCall("jwt"), authorization(["user", "premium"]), cartController.cartPurchase)
 
-router.delete("/:cid", passportCall("jwt"), authorization("user"), cartController.deleteProductsInCart)
+router.delete("/:cid", passportCall("jwt"), authorization(["user", "premium"]), cartController.deleteProductsInCart)
 
 module.exports = router
