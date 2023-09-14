1b4f689c70c6bb1d378f2a577116937147fc896e
andresaguiarok <andresaguiarok@outlook.com>
2023-09-13 21:38:33 -0300

    cambio de variable

--- src/public/javascript/cart.js
+++ src/public/javascript/cart.js
@@ -30,9 +30,20 @@ const purchase = async(id) => {
     })
 
     const responseJson = await response.json()
+    const ticket = responseJson.toTicket
 
     response.ok
-    ? Swal.fire({ icon: 'success', title: responseJson.message, showConfirmButton: false, timer: 1500 })
+    ? Swal.fire({
+        title: responseJson.message,
+        icon: 'success',
+        html:`            
+            <div>
+                <b>date of purchase : ${ticket.purchaseDatetime} </b>
+                <b>amount: ${ticket.amount} </b>
+            </div>`,
+        showCloseButton: true,
+        showCancelButton: true,
+      })
         && setTimeout(() => { location.reload() }, 2000)
     : Swal.fire({ icon: 'error', title: responseJson.status, text: responseJson.message })
 }
